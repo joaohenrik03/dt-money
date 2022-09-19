12e317ebf39605e6dd61306389a7080d3e025675
João Henrik <jh.signori@gmail.com>
2022-09-19 12:01:53 -0300

    Header component created and styled.

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+
+import logoImg from '../../assets/logo.svg';
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img 
+          src={logoImg}
+          alt="Logo DT Money" 
+        />
+
+        <NewTransactionButton>
+            Nova Transação
+        </NewTransactionButton>  
+      </HeaderContent>
+    </HeaderContainer> 
+  )
+}
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  background-color: ${props => props.theme["gray-900"]};
+  padding: 4rem 0 12rem;
+`;
+
+export const HeaderContent = styled.div`
+  width: 100%;
+  max-width: 1120px;
+  margin: 0 auto;
+  padding: 0 2.4rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const NewTransactionButton = styled.button`
+  height: 5rem;
+  padding: 0 2.4rem;
+
+  font-weight: bold;
+  color: ${props => props.theme.white};
+
+  border: 0;
+  border-radius: 6px;
+  background-color: ${props => props.theme["green-500"]};
+  cursor: pointer;
+
+  transition: background-color 0.2s;
+
+  &:hover {
+    background-color: ${props => props.theme["green-700"]};
+  }
+`;
