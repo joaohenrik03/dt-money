2b1e38ec9aa2c4f219b7ad4bba13dddb6d7e47e6
João Henrik <jh.signori@gmail.com>
2022-09-22 00:24:07 -0300

    Added function to calculate sum of inputs and outputs.

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -4,8 +4,30 @@ import {
   ArrowCircleUp, 
   CurrencyDollar,
 } from 'phosphor-react';
+import { useContext } from "react";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 
 export function Summary() {
+  const { transactions } = useContext(TransactionsContext);
+
+  // Estudar mais sobre a função reduce, muito bacana!!!
+
+  const summary = transactions.reduce((acc, transaction) => {
+    if (transaction.type === 'income') {
+      acc.income += transaction.price;  
+      acc.total += transaction.price;  
+    } else {
+      acc.outcome += transaction.price;  
+      acc.total -= transaction.price;  
+    }
+
+    return acc;
+  }, { 
+    income: 0, 
+    outcome: 0, 
+    total: 0 
+  })
+
   return (
     <SummaryContainer>
       <SummaryCard>
@@ -15,7 +37,7 @@ export function Summary() {
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>R$ {summary.income}</strong>
       </SummaryCard>   
 
       <SummaryCard>
@@ -25,7 +47,7 @@ export function Summary() {
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>R$ {summary.outcome}</strong>
       </SummaryCard>  
 
       <SummaryCard variant="green">
@@ -35,7 +57,7 @@ export function Summary() {
           <CurrencyDollar size={32} color="#fff" />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>R$ {summary.total}</strong>
       </SummaryCard>  
     </SummaryContainer>
   )
